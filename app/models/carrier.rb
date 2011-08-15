e46cdde56fc8509516b01a7f616361719d507542
Dan Melton <melton.dan@gmail.com>
2011-08-14 17:13:53 -0700

    Removed a bunch of uneeded files

--- app/models/carrier.rb
+++
@@ -1,7 +0,0 @@
-class Carrier < ActiveRecord::Base
-  has_many :text_sessions
-  attr_accessible :name, :carrierid
-  
-  validates_uniqueness_of :carrierid, :on => :create, :message => "must be unique"
-  
-end
--- db/migrate/20110814014404_drop_account_id_from_categories.rb
+++
@@ -1,9 +0,0 @@
-class DropAccountIdFromCategories < ActiveRecord::Migration
-  def self.up
-    remove_column :categories, :account_id
-  end
-
-  def self.down
-    add_column :categories, :account_id, :integer
-  end
-end
