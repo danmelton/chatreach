0995962b1244b824026b0a86ba4315b25cb9a831
Dan Melton <melton.dan@gmail.com>
2011-08-19 18:14:17 -0700

    Added text content new and edit form

--- app/controllers/text_contents_controller.rb
+++ app/controllers/text_contents_controller.rb
@@ -4,19 +4,38 @@ class TextContentsController < InheritedResources::Base
   before_filter :brand_admin, :only => [:create, :new, :destroy, :update, :edit]    
   layout "application"
   
+  def index
+    @text_contents = TextContent.where(:brand_id => session[:brand])
+  end
+  
   def update
-    update! {edit_text_content_path(@text_content)}
+    @text_content = TextContent.where(:id => params[:id]).first
+    if @text_content.update_attributes(params[:text_content]) 
+      flash[:success] = "Text content was updated!"
+      redirect_to edit_text_content_path(@text_content)
+    else
+      flash[:error] = "We couldnt save it the brand, category and tag combination!"
+      redirect_to edit_text_content_path(@text_content)
+    end
+    
   end
   
   def create
-    create! {edit_text_content_path(@text_content)}
+    @text_content = TextContent.new(params[:text_content]) 
+    if @text_content.save
+      flash[:success] = "Text content was added!"      
+      redirect_to edit_text_content_path(@text_content)
+    else
+      flash[:error] = "We couldnt save it the brand, category and tag combination!"      
+      redirect_to new_text_content_path(@text_content)
+    end
   end
       
   private
     
   def brand_admin
     if !admin? 
-      brand = Brand.find(session[:brand])
+      @brand = Brand.find(session[:brand])
       if !brand.admins.include?(current_user)
         @brand_admin = true 
         redirect_to :back
--- app/models/text_content.rb
+++ app/models/text_content.rb
@@ -4,5 +4,5 @@ class TextContent < ActiveRecord::Base
   belongs_to :category, :class_name => "Category", :foreign_key => "category_id"
   
   validates_presence_of :category, :brand, :response
-  validates_uniqueness_of :tag_id, :scope => [:category_id, :brand_id]
+  validates_uniqueness_of :tag_id, :scope => [:category_id, :brand_id], :message => "Tag and Action combinaation must be unique"
 end
