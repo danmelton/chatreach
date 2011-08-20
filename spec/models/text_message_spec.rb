8f77b26d0e9617dda7f051b0ebbe04ef8bce64b1
Dan Melton <melton.dan@gmail.com>
2011-08-19 22:03:49 -0700

    starting tm spec

---
+++ spec/models/text_message_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe TextMessage do
+  
+  context "intializes" do
+    
+    it 'finds the chatter' do
+    end
+    
+    it 'creates a chatter' do
+    end
+    
+    
+  end
+  
+  
+  
+end
